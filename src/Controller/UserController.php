63ea0ba79d61e086ef7b9042864b46d4c60a3828
Aymen Hmani <47940043+TheLime1@users.noreply.github.com>
2025-02-12 11:32:37 +0100

    cleaning extra files

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,22 +8,6 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController
 {
-    #[Route('/user/signup', name: 'singup')]
-    public function signup(): Response
-    {
-        return $this->render('user/signup.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
-    }
-
-    #[Route('/user/login', name: 'login')]
-    public function login(): Response
-    {
-        return $this->render('user/login.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
-    }
-
     #[Route('/user/profile', name: 'profile')]
     public function profile(): Response
     {
