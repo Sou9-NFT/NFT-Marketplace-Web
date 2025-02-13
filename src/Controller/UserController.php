7b01b2346bccd4fe7c202a3dbb84647b0382b2ea
Aymen Hmani <47940043+TheLime1@users.noreply.github.com>
2025-02-13 14:29:33 +0100

    Add profile picture upload functionality and remove old command

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,10 +10,19 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\String\Slugger\SluggerInterface;
+use Doctrine\ORM\EntityManagerInterface;
 
 #[Route('/user')]
 class UserController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/', name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
@@ -50,20 +59,50 @@ class UserController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    public function edit(Request $request, User $user, UserRepository $userRepository, SluggerInterface $slugger): Response
     {
         $currentUser = $this->getUser();
         if (!$currentUser instanceof User || $currentUser->getId() !== $user->getId()) {
-            throw new AccessDeniedException('You are not allowed to edit this //TODO: make it pretty profile.'); //TODO: make it pretty
+            throw new AccessDeniedException('You are not allowed to edit this profile.');
         }
 
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $userRepository->save($user, true);
+            try {
+                $profilePictureFile = $form->get('profilePicture')->getData();
 
-            return $this->redirectToRoute('app_user_show', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
+                if ($profilePictureFile) {
+                    $originalFilename = pathinfo($profilePictureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                    $safeFilename = $slugger->slug($originalFilename);
+                    $newFilename = $safeFilename . '-' . uniqid() . '.' . $profilePictureFile->guessExtension();
+
+                    $uploadsDirectory = $this->getParameter('profile_pictures_directory');
+                    if (!file_exists($uploadsDirectory)) {
+                        mkdir($uploadsDirectory, 0777, true);
+                    }
+
+                    $profilePictureFile->move($uploadsDirectory, $newFilename);
+
+                    $oldFilename = $user->getProfilePicture();
+                    if ($oldFilename) {
+                        $oldFilePath = $uploadsDirectory . '/' . $oldFilename;
+                        if (file_exists($oldFilePath)) {
+                            unlink($oldFilePath);
+                        }
+                    }
+
+                    $user->setProfilePicture($newFilename);
+                    $this->entityManager->persist($user);
+                    $this->entityManager->flush();
+                }
+
+                $userRepository->save($user, true);
+                return $this->redirectToRoute('app_user_show', ['id' => $user->getId()], Response::HTTP_SEE_OTHER);
+            } catch (\Exception $e) {
+                $this->addFlash('error', 'Error updating profile picture.');
+            }
         }
 
         return $this->renderForm('user/edit.html.twig', [
