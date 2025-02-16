b616c6954cb0f161c21958cd6683d87ee3133555
sanalar00 <sana.lar0015@gmail.com>
2025-02-16 14:06:36 +0100

    BACKDONE

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Blog;
 use App\Entity\Comment;
+use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -15,6 +16,12 @@ class CommentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('user', EntityType::class, [
+                'class' => 'App\Entity\User',
+                'choice_label' => 'name',
+                'label' => 'User',
+                'disabled' => true,
+            ])
             ->add('content', TextareaType::class, [
                 'label' => 'Your Comment',
                 'attr' => ['rows' => 4, 'placeholder' => 'Write your comment here...'],
