5d4d852fee447c661da835bf11970c42b22530e5
Aymen Hmani <47940043+TheLime1@users.noreply.github.com>
2025-02-28 20:45:05 +0100

    email send (not tested the pass reset yet)

--- src/Service/PasswordResetService.php
+++ src/Service/PasswordResetService.php
@@ -9,7 +9,7 @@ use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Address;
 use Symfony\Component\Security\Core\Exception\UserNotFoundException;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\HttpFoundation\RequestStack;
 
@@ -18,7 +18,7 @@ class PasswordResetService
     private $mailer;
     private $entityManager;
     private $userProvider;
-    private $passwordEncoder;
+    private $passwordHasher;
     private $urlGenerator;
     private $requestStack;
 
@@ -26,23 +26,23 @@ class PasswordResetService
         MailerInterface $mailer,
         EntityManagerInterface $entityManager,
         UserProviderInterface $userProvider,
-        UserPasswordEncoderInterface $passwordEncoder,
+        UserPasswordHasherInterface $passwordHasher,
         UrlGeneratorInterface $urlGenerator,
         RequestStack $requestStack
     ) {
         $this->mailer = $mailer;
         $this->entityManager = $entityManager;
         $this->userProvider = $userProvider;
-        $this->passwordEncoder = $passwordEncoder;
+        $this->passwordHasher = $passwordHasher;
         $this->urlGenerator = $urlGenerator;
         $this->requestStack = $requestStack;
     }
 
     public function sendPasswordResetEmail(string $email): void
     {
-        $user = $this->userProvider->loadUserByUsername($email);
-
-        if (!$user) {
+        try {
+            $user = $this->userProvider->loadUserByIdentifier($email);
+        } catch (\Exception $e) {
             throw new UserNotFoundException(sprintf('No user found with email "%s"', $email));
         }
 
@@ -59,7 +59,7 @@ class PasswordResetService
             ->from(new Address('no-reply@example.com', 'NFT Marketplace'))
             ->to($user->getEmail())
             ->subject('Your password reset request')
-            ->htmlTemplate('emails/password_reset.html.twig')
+            ->htmlTemplate('security/password_reset_email.html.twig')
             ->context([
                 'resetUrl' => $resetUrl,
                 'user' => $user,
@@ -81,8 +81,8 @@ class PasswordResetService
 
     public function resetPassword(User $user, string $newPassword): void
     {
-        $encodedPassword = $this->passwordEncoder->encodePassword($user, $newPassword);
-        $user->setPassword($encodedPassword);
+        $hashedPassword = $this->passwordHasher->hashPassword($user, $newPassword);
+        $user->setPassword($hashedPassword);
         $user->setPasswordResetToken(null);
         $user->setPasswordResetTokenExpiresAt(null);
 
